d0613d2e8f819a43808341252ee9b473f38f2af9
Ahmed Magdy <ahmedmagdy2016@gmail.com>
2021-10-28 12:38:13 +0200

    add timestamps fields to checks model

--- models/checks.js
+++ models/checks.js
@@ -1,75 +1,80 @@
 const { Schema, model } = require('mongoose')
 
-const checkSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  url: {
-    type: String,
-    required: true
-  },
-  protocol: {
-    type: String,
-    enum: ['HTTP', 'HTTPS', 'TCP'],
-    required: true
-  },
-  userId: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ['Paused', 'Running'],
-    default: 'Running'
-  },
-  path: {
-    type: String
-  },
-  port: {
-    type: Number
-  },
-  webhook: {
-    type: String
-  },
-  timeout: {
-    type: Number,
-    default: 5
-  },
-  interval: {
-    type: Number,
-    default: 10
-  },
-  threshold: {
-    type: Number,
-    default: 1
-  },
-  authentication: {
-    username: {
+const checkSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    protocol: {
+      type: String,
+      enum: ['HTTP', 'HTTPS', 'TCP'],
+      required: true
+    },
+    userId: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ['Paused', 'Running'],
+      default: 'Running'
+    },
+    path: {
       type: String
     },
-    password: {
-      type: String
-    }
-  },
-  httpHeaders: [
-    {
-      key: String,
-      value: String
-    }
-  ],
-  assert: {
-    statusCode: {
+    port: {
       type: Number
+    },
+    webhook: {
+      type: String
+    },
+    timeout: {
+      type: Number,
+      default: 5
+    },
+    interval: {
+      type: Number,
+      default: 10
+    },
+    threshold: {
+      type: Number,
+      default: 1
+    },
+    authentication: {
+      username: {
+        type: String
+      },
+      password: {
+        type: String
+      }
+    },
+    httpHeaders: [
+      {
+        key: String,
+        value: String
+      }
+    ],
+    assert: {
+      statusCode: {
+        type: Number
+      }
+    },
+    tags: {
+      type: [String]
+    },
+    ignoreSSL: {
+      type: Boolean
     }
   },
-  tags: {
-    type: [String]
-  },
-  ignoreSSL: {
-    type: Boolean
+  {
+    timestamps: true
   }
-})
+)
 
 const checkModel = model('checks', checkSchema)
 
